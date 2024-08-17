4bc4c3256c42395d38037ef908bba666815dd67e
pongpetr <suerdragon8995@gmail.com>
2024-08-17 22:28:33 +0700

    bp3-pang

--- ex-3.js
+++ ex-3.js
@@ -374,4 +374,8 @@ const bills = [
 ];
 
 // Start coding here
-const billMembers;
+const billMembers = bills.filter((bill) => {
+  return bill.member !== null;}).map((bill) => {
+    return bill.member.name;
+  });
+  console.log(billMembers)
